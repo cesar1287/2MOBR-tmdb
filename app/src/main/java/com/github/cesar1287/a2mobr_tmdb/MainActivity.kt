4c6ba11244d7ca4a9155da031c25be44536e23b2
Cesar Rodrigues <cesar.nascimento.ufc@gmail.com>
2023-09-27 19:32:57 -0300

    Utilização do ViewBinding

--- app/src/main/java/com/github/cesar1287/a2mobr_tmdb/MainActivity.kt
+++ app/src/main/java/com/github/cesar1287/a2mobr_tmdb/MainActivity.kt
@@ -2,11 +2,16 @@ package com.github.cesar1287.a2mobr_tmdb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.github.cesar1287.a2mobr_tmdb.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
